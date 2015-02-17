d35ee75441831aba0dde46ec8957af4a24c6a1cb
Peter de Croos <cultofmetatron@aumlogic.com>
2015-02-16 19:49:34 -0800

    signup and login forms

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,13 +1,22 @@
 
-(function() {
+angular.module('app', ['ui.bootstrap'])
+.controller('TabsAccounts', function ($scope, $window) {
+  $scope.loginHandler = function() {
+    console.log('login', arguments);
+  };
+
+  $scope.signupHandler = function() {
+    console.log('signup', arguments);
+  
+  };
   
 
+});
 
 
 
 
 
-}).call(this);
 
 
 
